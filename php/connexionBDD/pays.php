448d2bdf5a78c3d59a4d30daba6673a457a29831
magavel <magavel@mac.com>
2018-10-09 21:28:03 +0200

    maj pays.php ajout des boutons esc et desc

--- php/connexionBDD/pays.php
+++ php/connexionBDD/pays.php
@@ -14,13 +14,13 @@ Un lien sur le nom du pays enverra sur le détail du pays avec toutes les infos 
 
 */
 
-
+//test
 
 try{
     $bdd = new PDO(
         'mysql:host=localhost;dbname=miw;charset=utf8',
         'root',
-        '',
+        'root',
         array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING)
     );
 }catch(Exception $e){
@@ -39,7 +39,10 @@ where code=:pays');
     $infoPays->execute( array('pays'=>$pays));
     while ($row=$infoPays->fetch()){
         echo '<h1>Pays choisi: '.$row['name'].'</h1>';
-        echo 'Continent: '.$row['continent'];
+        echo '<p>Continent: '.$row['continent'].'</p>';
+        echo '<p>Gouvernement: '.$row['governmentform'].'</p>';
+        echo '<p>Surface du pays: '.$row['surfacearea'].'</p>';
+
     }
 
     $listeVilles = $bdd->prepare('select city.name as name
@@ -58,24 +61,41 @@ order by city.population desc limit 0,10');
     echo '</table><br><br>';
 }
 
+?>
 
 
+<form method="post" action="pays.php">
+    <input type='submit' name='ASC' value='Tri ascendant'/>
+    <input type='submit' name='DESC' value='Tri descendant'/>
+</form>
 
+<?php
+$trisAsc=$_POST['ASC'];
 
-
-
-$ordre = 'ASC';
-$req = $bdd ->prepare ('select country.code as codePays, country.name as name, country.population as population, count(city.name) as nbreVille 
-                from country as country 
+$requete='select country.code as codePays, country.name as name, country.population as population, count(city.name) as nbreVille
+                from country as country
                 inner join city as city on country.code = city.countrycode
-                group by country.code 
-                order by :ordre');
+                group by country.code';
+$tri= ' country.name';
 
-$req ->bindValue('ordre', $ordre, PDO::PARAM_STR);
-$req ->execute(array('ordre'=>$ordre));
+//$ordreTri=trier;
+
+if(isset($_POST['ASC'])&&!empty($_POST['ASC'])){
+    $requete.=' ORDER BY '.$tri;
+
+}if (isset($_POST['DESC'])&&!empty($_POST['DESC'])){
+    $requete.= ' ORDER by '.$tri.' DESC';
+}else{
+    $requete=$requete;
+}
+
+$req=$bdd->prepare($requete);
+
+$req ->execute();
 
 echo '<table>';
 while ($row = $req->fetch()) {
+
     echo '<tr> <td>pays: ';
     echo '<a href="pays.php?pays='.$row['codePays'].'">'.$row['name'] .'</a></td><td>'.' population: '.$row['population'].' </td><td>Nbre de villes: '.$row['nbreVille'].'</td></tr>';
 }
