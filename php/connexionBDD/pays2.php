08c46913a8c2812827b39a74ff3ca76e362271c1
magavel <magavel@mac.com>
2018-10-21 07:20:34 +0200

    maj

--- php/connexionBDD/pays2.php
+++ php/connexionBDD/pays2.php
@@ -8,41 +8,14 @@
 
 
 
-try{
-    $bdd =new PDO(
-        'mysql:host=localhost;dbname=miw;charset=utf8',
-        'root',
-        'root'
-    );
-}catch (Exception $e){
-    die('Erreur: '.$e->getMessage());
-}
 
-
-//requete pour recuperer la liste des pays avec le nom du pays, la population, les 10 plus grande ville
-
-$reqSql='select country.name as pays, country.population as pop, count(city.name) as nbreVille
-from country
-       inner join city on country.code = city.countrycode
-group by country.code';
-
-$req = $bdd->prepare($reqSql);
-$req->execute();
-?>
-
-<table>
-<?php
-while ($row =$req->fetch()){
-    ?>
-    <tr><td>
-    <?php
-    echo $row['pays'].'<td>'.$row['pop'].'</td>'.'<td>'.$row['nbreVille'].'</td>';
-
-    ?>
-        </td></tr>
-
-    <?php
-}
-
-?>
-</table>
+ try{
+     $bdd= new PDO(
+         'mysql:host=localhost;dbname=miw;charset=utf8',
+         'root',
+         'root',
+         array(PDO::ATTR_ERRMODE=>PDO::ATTR_ERRMODE_WARNING)
+     );
+ }catch(Exception $e){
+     die('Erreur: '.$e->getMessage));
+ }
